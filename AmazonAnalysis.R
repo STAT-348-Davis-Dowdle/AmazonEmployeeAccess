8e72d6cc5e0591c7b24d16fd16fcd1941d53f421
Davis Dowdle <davis.dowdle@gmail.com>
2023-10-11 13:45:12 -0600

    Penalized Logistic Regression Update

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -45,3 +45,39 @@ log_submission <- data.frame(id = test$id,
                              ACTION = log_prediction$.pred_1)
 
 write.csv(log_submission, "log_submission.csv", row.names = F)
+
+
+# penalized logistic regression
+
+pen_log_model <- logistic_reg(mixture = tune(),
+                              penalty = tune()) %>%
+  set_engine("glmnet")
+
+pen_log_workflow <- workflow() %>%
+  add_recipe(recipe) %>%
+  add_model(pen_log_model)
+
+pen_log_tuning_grid <- grid_regular(penalty(),
+                                    mixture(),
+                                    levels = 5)
+
+pen_log_folds <- vfold_cv(train, v = 5, repeats = 1)
+
+pen_cv_results <- pen_log_workflow %>%
+  tune_grid(resamples = pen_log_folds,
+            grid = pen_log_tuning_grid,
+            metrics = metric_set(roc_auc))
+
+pen_log_besttune <- pen_cv_results %>%
+  select_best("roc_auc")
+
+final_pen_log_workflow <- pen_log_workflow %>%
+  finalize_workflow(pen_log_besttune) %>%
+  fit(data = train)
+
+pen_log_predictions <- final_pen_log_workflow %>%
+  predict(new_data = test, type = "prob")
+
+pen_log_submission <- data.frame(id = test$id, ACTION = pen_log_predictions$.pred_1)
+
+write.csv(pen_log_submission, "pen_log_submission.csv", row.names = F)
