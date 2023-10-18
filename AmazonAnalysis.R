3835930e5fb8b9bbd342c3e265945f6cdfa81722
Davis Dowdle <davis.dowdle@gmail.com>
2023-10-18 13:50:28 -0600

    Naive Bayes

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -2,6 +2,8 @@ library(vroom)
 library(patchwork)
 library(tidymodels)
 library(embed)
+library(discrim)
+library(naivebayes)
 
 # set working directory
 setwd("C:/Users/davis/OneDrive - Brigham Young University/Documents/skool/new/stat 348/AmazonEmployeeAccess/AmazonEmployeeAccess")
@@ -118,3 +120,44 @@ class_rf_predictions <- final_class_rf_wf %>%
 class_rf_submission <- data.frame(id = test$id, ACTION = class_rf_predictions$.pred_class)
 
 write.csv(class_rf_submission, file = "class_rf_submission.csv", row.names = F)
+
+
+# Naive Bayes
+
+recipe <- recipe(ACTION ~ ., data = train) %>%
+  step_mutate_at(all_numeric_predictors(), fn = factor) %>%
+  step_other(all_nominal_predictors(), threshold = .01) %>% # all uncommon categories for all variables converted to other
+  # step_dummy(all_nominal_predictors()) %>% 
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(ACTION))
+
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+nb_wf <- workflow() %>%
+  add_recipe(recipe) %>%
+  add_model(nb_model)
+
+nb_tuning_grid <- grid_regular(Laplace(),
+                               smoothness(),
+                               levels = 3)
+
+nb_folds <- vfold_cv(train, v = 5, repeats = 1)
+
+nb_results <- nb_wf %>%
+  tune_grid(resamples = nb_folds,
+            grid = nb_tuning_grid)
+
+best_nb_wf <- nb_results %>%
+  select_best("roc_auc")
+
+final_nb_wf <- nb_wf %>%
+  finalize_workflow(best_nb_wf) %>%
+  fit(data = train)
+
+nb_prediction <- final_nb_wf %>%
+  predict(new_data = test, type = "prob")
+
+nb_submission <- data.frame(id = test$id, ACTION = nb_prediction$.pred_1)
+
+write.csv(nb_submission, "nb_submission.csv", row.names = F)
